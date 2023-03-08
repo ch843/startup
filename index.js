da260a0794be1081cc209bbecb7398c87ecbf4c4
ch843 <cassidy.hardisty@gmail.com>
2023-03-08 13:12:45 -0700

    updated .env

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 async function getTacoShop() {
     var requestOptions = {
         method: 'GET',
@@ -7,19 +9,24 @@ async function getTacoShop() {
       fetch("https://nksou7u3c4.execute-api.us-east-2.amazonaws.com/default/taco_selector", requestOptions)
         .then(response => response.text())
         .then(result => {
-            console.log(getShopInfo(result))
+            const info = getShopInfo(result)
+            console.log(info)
         })
         
         .catch(error => console.log('error', error));
 }
 
+// delete this function and use functions from Places API
 async function getShopInfo(result) {
     var requestOptions = {
         method: 'GET',
-        redirect: 'follow'
+        redirect: 'follow',
+        // headers: {
+        //   'Access-Control-Allow-Origin': 
+        // }
       };
-      fetch(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?key=${}&query=${result}`, requestOptions)
+        fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?key=AIzaSyA6FcFpZL3u2GZeQ1o9dTw4rKpAewq7AQA&query=${result}`, requestOptions)
         .then(response => response.text())
-        .then(result => console.log(result))
+        .then(result => result)
         .catch(error => console.log('error', error));
 }
